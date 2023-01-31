a9a4710e08751245a49f809cdbe0c0e337c55ccd
Camila <contactcamilasanchez@gmail.com>
2023-01-30 22:33:25 -0300

    dinamic articles and first banner

---
+++ data/articles.js
@@ -0,0 +1,50 @@
+const articles = [
+    {
+        id: 1,
+        img: "https://icatcare.org/app/uploads/2018/07/Helping-your-new-cat-or-kitten-settle-in-1.png",
+        img_description: "Tiny kitten",
+        title: "A super important article title about Kittens.",
+        abstract: "Lorem ipsum dolor sit amet. Quibusdam magnam dicta, sit ex explicabo, suscipit perferendis porro eveniet cumque nostrum sapiente dolorem exercitationem eius fuga inventore a alias, temporibus pariatur.",
+        main_article: ""
+    },
+    {
+        id: 2,
+        img: "https://images.ctfassets.net/j4x7ugudy4uu/14fHrENjRw66EORUnqHNEI/0c74e3239fe3cd1d9a704de26b30b0be/09.01.20_Testimonial_Images_HP668X668_01.jpg",
+        img_description: "Dog playing with toys",
+        title: "A super important article title about Dogs.",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam magnam dicta, sit ex explicabo, suscipit perferendis porro eveniet cumque nostrum sapiente dolorem exercitationem eius fuga inventore a alias, temporibus pariatur.",
+        main_article: ""
+    },
+    {
+        id: 3,
+        img: "https://media.npr.org/assets/img/2022/11/23/russian-toy-2-3-_custom-fd300880a9643efca73031d33f38ca7f4054b710.jpg",
+        img_description: "Brown chihuahua",
+        title: "A super important article title about Dogs.",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam magnam dicta, suscipit perferendis porro eveniet exercitationem eius fuga inventore a alias, temporibus pariatur.",
+        main_article: ""
+    },
+    {
+        id: 4,
+        img: "https://ggsc.s3.amazonaws.com/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner.jpg",
+        img_description: "Happy dog",
+        title: "A super important article title about Dogs.",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam magnam dicta, sit ex explicabo, suscipit perferendis fuga inventore a alias, temporibus pariatur.",
+        main_article: ""
+    },
+    {
+        id: 5,
+        img: "http://cdn.akc.org/content/article-body-image/samoyed_puppy_and_girl_.jpg",
+        img_description: "A girl and a white dog",
+        title: "A super important article title about Dogs.",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam magnam dicta, sit ex explicabo, suscipit perferendis porro eveniet cumque nostrum sapiente dolorem exercitationem eius fuga inventore a alias, temporibus pariatur.",
+        main_article: ""
+    },
+    {
+        id: 6,
+        img: "https://as1.ftcdn.net/v2/jpg/02/56/25/56/1000_F_256255660_jGDw9XV8MB8AXfcDRjvi0dXRyf7CbWdB.jpg",
+        img_description: "Funny cat",
+        title: "A super important article title about Cats.",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam magnam dicta, sit ex explicabo, suscipit perferendis porro eveniet cumque nostrum sapiente dolorem exercitationem eius fuga inventore a alias, temporibus pariatur.",
+        main_article: ""
+    },
+]
---
+++ script.js
@@ -0,0 +1,24 @@
+const firstColumn = document.getElementById("firstColumn");
+const secondColumn = document.getElementById("secondColumn");
+const thirdColumn = document.getElementById("thirdColumn");
+
+const getColumn = (index) => {
+  if (index % 3 === 0) return firstColumn;
+  if (index % 3 === 1) return secondColumn;
+  return thirdColumn;
+};
+
+const renderArticlePreview = (column, img, imgDescription, title, abstract) => {
+  const articlePreview = `<div class="preview-art-container">
+    <img class="preview-art-image" src="${img}" alt="${imgDescription}"/>
+    <h2 class="title preview-art-title">${title}</h2>
+    <p>${abstract}</p>
+  </div>`;
+
+  column.innerHTML += articlePreview;
+};
+
+articles.forEach(({ img, img_description, title, abstract }, index) => {
+  const column = getColumn(index);
+  renderArticlePreview(column, img, img_description, title, abstract);
+});
