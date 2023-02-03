04b580d3a7cf317845942b19a761b8c41b71c5c0
Camila <contactcamilasanchez@gmail.com>
2023-02-03 17:25:57 -0300

    basic article page

--- scripts/article.js
+++ scripts/article.js
@@ -2,11 +2,11 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
-const id = parseInt(urlParams.get('id'))
+const articleId = parseInt(urlParams.get('id'))
 
 // Get article with id
 // Articles array is in ../data/articles.js
-const article = articles.find((art) => art.id === id)
+const article = articles.find((art) => art.id === articleId)
 
 // Get HTML article elements
 const title = document.getElementById("title")
@@ -26,3 +26,34 @@ image.alt = article.img_description
 
 // Set article mainArticle
 mainArticle.innerText = article.main_article
+
+// Get other articles column
+const articlesColumn = document.getElementById("articlesColumn")
+
+// Function to add the article to the HTML
+const renderArticlePreview = (
+    img,
+    imgDescription,
+    title,
+    abstract,
+    id
+  ) => {
+    const articlePreview = `
+    <a href="./article.html?id=${id}" class="preview-art-container">
+      <img class="preview-art-image" src="${img}" alt="${imgDescription}"/>
+      <h2 class="title preview-art-title">${title}</h2>
+      <p>${abstract}</p>
+    </a>`;
+  
+    articlesColumn.innerHTML += articlePreview;
+};
+
+// Render first five articles
+// The articles array is defined in data/articles.js
+for (let i = 0; i < 5; i++) {
+    const {id, img, img_description, title, abstract} = articles[i]
+    // Add it to the HTML if it is not the main article
+    if (id !== articleId) {
+        renderArticlePreview(img, img_description, title, abstract, id);
+    }
+}
