1acf916564f59a6b25230bae6a44ddaa38de1dd7
Camila <contactcamilasanchez@gmail.com>
2023-02-25 19:24:57 -0300

    two articles

--- data/articles.js
+++ data/articles.js
@@ -35,9 +35,9 @@ const articles = [
     img_description: "Happy dog",
     title: "Most popular dog breeds this 2023.",
     abstract:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam magnam dicta, sit ex explicabo, suscipit perferendis fuga inventore a alias, temporibus pariatur.",
-    main_article: "",
-    tags: [],
+      "These are the dog breeds that will be most popular with new and old adopters, this year 2023.",
+    main_article: "<strong> 1. Labrador Retriever </strong> <br> He is loyal, protective, understanding and loves his loved ones with all his heart. <br> <br><strong> 2. German Shepherd </strong> <br> Although it is a relatively new breed, dogs of this breed are incredible specimens, they are brave, obedient and very loyal. They are also very intelligent and that is why they are commonly service dogs or police dogs. <br> <br><strong> 3. Newfoundland </strong> <br> These dogs are the sweetest dogs we will come across, they are gentle and loyal. For this reason they are excellent for families with children. <br> <br><strong> 4. Maltese </strong> <br> These dogs were bred to be loving and adore their owners. In ancient Greece it was believed that they relieved pain and could cure diseases. <br> <br><strong> 5. Golden Retriever </strong> <br> This is a very popular breed of dog for being sweet and loyal, they will never leave your side and have a lot of energy.",
+    tags: ['2023', 'dog breeds', 'popular', 'adopt'],
   },
   {
     id: 5,
@@ -46,8 +46,8 @@ const articles = [
     title: "Is your dog just like you?",
     abstract:
       "Some recent studies have managed to show that our personality is directly reflected in our pets. Is this your case too?",
-    main_article: "",
-    tags: [],
+    main_article: "Recent research indicates that there are shared traits between owners and their respective dogs. But how does it work? The study was conducted by William J.Chopik, a social psychologist at Michigan State University. <br><br> 1681 people were asked to rate their dogs' personalities and their own, and the results showed the obvious similarity between the two.<br><br>First of all, part of this similarity is that owners unintentionally choose dogs similar to themselves, as they feel more connected to those who resemble them both in personality and physique. On the other hand, socializing and living together makes both personalities mix so to speak, taking both things from the other and thus ending up having similar personalities. <br><br> And finally, part of the similarity is also due to the fact that the owners project themselves onto their pets, so when they self-assessed themselves and their pets, the similarities were highlighted.",
+    tags: ['owners', 'dogs', 'personality', 'similar'],
   },
   {
     id: 6,
@@ -88,6 +88,6 @@ const articles = [
       "Going to the dog park is one of the favorite activities for these little ones, but it can become a nightmare if not done correctly.",
     main_article:
       "There are more and more parks dedicated to our dogs, areas designed 100% for them and their enjoyment, but are they for all dogs? We tell you the most common mistakes that owners make. <br> <br> <strong>1. Release the dog as soon as they enter.</strong> <br> Our pet can get too excited when we start to approach the park, an overly excited dog gets nervous and can end up fighting with the other dogs in the place. <br> <br> What should I do then? We must try to change the paths we take so that it does not identify our routine, and when we arrive at the park we will take a few minutes to reassure our pet. <br> <br> <strong>2. Bring our dog in heat.</strong> <br> It seems obvious but it is not for many, some certain phases of our pet's heat can end in aggressive behavior towards the rest of the dogs in the park, or generate disputes between the other dogs because they will fight for it. <br> <br> We can go during the last stage of our pet's heat since neither she is aggressive nor are the other dogs interested in her, but we must always take into account the supervision and care of everyone for this.<br> <br> <strong> 3. Bringing toys or food to the park </strong> <br> It is quite common to see owners bring treats to their beloved puppies or even their favorite toys. The problem with this is that some dogs can be selfish with their things, protecting their food or their toys. <br> <br> This again can lead to conflicts with other dogs. <br> For this reason we have to avoid bringing toys and when we want to give him some food, separate him and feed him in private. <br> Also, we should never feed a dog that is not our own, much less without asking beforehand. <br> <br>",
-    tags: ["mistakes", "dog parks", "dog"],
+    tags: ['mistakes', 'dog parks', 'dog'],
   },
 ];
