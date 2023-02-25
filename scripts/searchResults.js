1fd2e2f3c933f945f08d76aec53af5a1af10da44
Camila <contactcamilasanchez@gmail.com>
2023-02-25 20:34:07 -0300

    fix del fix hehe

--- scripts/searchResults.js
+++ scripts/searchResults.js
@@ -16,7 +16,7 @@ main.innerHTML += renderedSearchTitle;
 const result = articles.filter((article) => article.tags.find(a => a.includes(search)))
 
 // If there are no resulting articles, show text
-main.innerHTML += "<p>We could find any articles with your search.</p>";
+if (!result.length) main.innerHTML += "<p>We could find any articles with your search.</p>";
 
 // Get handlebars result article template
 const searchResult = document.getElementById("searchResultTemplate").innerHTML;
