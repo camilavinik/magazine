e0fff47e647c9074dbe7d83bcec205ec131a42f0
Camila <contactcamilasanchez@gmail.com>
2023-02-25 19:38:52 -0300

    superhero article

--- data/articles.js
+++ data/articles.js
@@ -75,9 +75,9 @@ const articles = [
     img_description: "Superhero dog",
     title: "Stories of our pets being our superheroes.",
     abstract:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam magnam dicta, sit ex explicabo, suscipit perferendis fuga inventore a alias, temporibus pariatur.",
-    main_article: "",
-    tags: [],
+      "Today we are going to tell the story of Chiquito, the dog that rescued his owner from a fire.",
+    main_article: "Its owner was peacefully sleeping while a short circuit generated began to make ashes in his home. Between the exhaustion and the toxic fumes from the fire, the man was sleeping in his bed.<br><br>Luckily, Chiquito somehow knew the danger they were running, so he forced his way into the room to wake up its owner.<br><br><strong>'He was outside, I don't know how he managed to break the window and get in, but he passed by and made me fall out of bed'</strong><br><br>Small achievement he dragged his owner to the dining room and made him wake up with blows and licks, when his owner became conscious, he was able to ask the neighbors for help to get out of the house, since he could not open the door.<br><br> Little after that, not enough to have managed to get out of the fire with its owner, he went back in to rescue the cat that also lived with the two of them, he took it in his mouth and ran outside.<br><br> Now the three meet safe thanks to Chiquito.",
+    tags: ['hero', 'dog', 'fire', 'superhero', 'cat', 'save', 'owner'],
   },
   {
     id: 9,
@@ -88,6 +88,6 @@ const articles = [
       "Going to the dog park is one of the favorite activities for these little ones, but it can become a nightmare if not done correctly.",
     main_article:
       "There are more and more parks dedicated to our dogs, areas designed 100% for them and their enjoyment, but are they for all dogs? We tell you the most common mistakes that owners make. <br> <br> <strong>1. Release the dog as soon as they enter.</strong> <br> Our pet can get too excited when we start to approach the park, an overly excited dog gets nervous and can end up fighting with the other dogs in the place. <br> <br> What should I do then? We must try to change the paths we take so that it does not identify our routine, and when we arrive at the park we will take a few minutes to reassure our pet. <br> <br> <strong>2. Bring our dog in heat.</strong> <br> It seems obvious but it is not for many, some certain phases of our pet's heat can end in aggressive behavior towards the rest of the dogs in the park, or generate disputes between the other dogs because they will fight for it. <br> <br> We can go during the last stage of our pet's heat since neither she is aggressive nor are the other dogs interested in her, but we must always take into account the supervision and care of everyone for this.<br> <br> <strong> 3. Bringing toys or food to the park </strong> <br> It is quite common to see owners bring treats to their beloved puppies or even their favorite toys. The problem with this is that some dogs can be selfish with their things, protecting their food or their toys. <br> <br> This again can lead to conflicts with other dogs. <br> For this reason we have to avoid bringing toys and when we want to give him some food, separate him and feed him in private. <br> Also, we should never feed a dog that is not our own, much less without asking beforehand. <br> <br>",
-    tags: ['mistakes', 'dog parks', 'dog'],
+    tags: ['mistakes', 'dog parks', 'dog', 'owners'],
   },
 ];
